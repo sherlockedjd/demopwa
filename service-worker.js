a7aaa164a1db9d7849724b007f9d2fa4ce4ac50e
sherlockedjd <saad.jawed@logic-unit.com>
2019-03-28 17:54:51 +0500

    changes made

--- service-worker.js
+++ service-worker.js
@@ -59,11 +59,30 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
     })
   );
 });
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
