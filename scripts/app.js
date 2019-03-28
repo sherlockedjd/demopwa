adcc2309daf654ffa935245d6818bc19c5407bc1
sherlockedjd <saad.jawed@logic-unit.com>
2019-03-28 16:16:45 +0500

    changed app.js file

--- scripts/app.js
+++ scripts/app.js
@@ -348,7 +348,7 @@
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('../service-worker.js')
+             .register('./service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
