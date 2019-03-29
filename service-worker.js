930fce9b3694c4db55b83adf71e981835ee1fe63
sherlockedjd <saad.jawed@logic-unit.com>
2019-03-29 15:12:34 +0500

    several-changes102

--- service-worker.js
+++ service-worker.js
@@ -49,6 +49,18 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  showAddToHomeScreen();
+
+});
+
 function fetchAndCache(url) {
   return fetch(url)
   .then(function(response) {
