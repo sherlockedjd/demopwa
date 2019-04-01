bb698a6e0629676348b7a75be6b0c109bc9a1917
sherlockedjd <saad.jawed@logic-unit.com>
2019-04-01 15:04:52 +0500

    service-worker -precache-11

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,8 @@
 // limitations under the License.
 
 var cacheName = 'weatherPWA-step-6-1';
-var filesToCache = ['/demopwa/scripts/app.js',
+var filesToCache = [
+   '/demopwa/scripts/app.js',
   '/demopwa/styles/inline.css',
   '/demopwa/images/clear.png',
   '/demopwa/images/cloudy-scattered-showers.png',
@@ -27,7 +28,8 @@ var filesToCache = ['/demopwa/scripts/app.js',
   '/demopwa/images/sleet.png',
   '/demopwa/images/snow.png',
   '/demopwa/images/thunderstorm.png',
-  '/demopwa/images/wind.png'];
+  '/demopwa/images/wind.png'
+  ];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
