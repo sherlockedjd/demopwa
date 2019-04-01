f0d500063611a8689224816ab5612ad9333eeab6
sherlockedjd <saad.jawed@logic-unit.com>
2019-04-01 14:23:11 +0500

    service-worker -precache-2

--- service-worker.js
+++ service-worker.js
@@ -37,6 +37,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
+      debugger;
       return cache.addAll(filesToCache);
     })
   );
